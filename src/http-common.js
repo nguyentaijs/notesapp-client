e68c97a641f36dd9dc1e56738fc19960d8aa8db3
TaiNT <ca_admin@kt.com>
2021-09-02 14:04:57 +0700

    Update server side url

--- src/http-common.js
+++ src/http-common.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:8080/api",
+    baseURL: "https://notesapp-nguyentaijs.herokuapp.com/api",
     headers: {
         "Content-type": "application/json"
     }
